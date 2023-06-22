cb4031f990718b70288695a4d1e8851f346cd56e
@johnya0 <anoopjohny2000@gmail.com>
2023-06-22 16:12:55 +0200

    commit

--- main.py
+++ main.py
@@ -42,18 +42,222 @@ def predict_sentiment(text, language, threshold_positive, threshold_negative):
             entities.append(entity.text)
         sentiment_scores = sid.polarity_scores(text)
         sentiment_scores["entities"] = entities
-    elif language == "es":
-        # Perform sentiment analysis in Spanish with entity recognition
-        pass
-    elif language == "af":
-        # Perform sentiment analysis in Afrikaans with entity recognition
-        pass
-    elif language == "ar":
-        # Perform sentiment analysis in Arabic with entity recognition
-        pass
-    elif language == "bg":
-        # Perform sentiment analysis in Bulgarian with entity recognition
-        pass
+
+    elif language == 'es':
+        sid_spanish = SentimentIntensityAnalyzer()
+        sentiment_scores = sid_spanish.polarity_scores(text)
+    elif language == 'af':
+        sid_afrikaans = SentimentIntensityAnalyzer()
+        sentiment_scores = sid_afrikaans.polarity_scores(text)
+    elif language == 'ar':
+        sid_arabic = SentimentIntensityAnalyzer()
+        sentiment_scores = sid_arabic.polarity_scores(text)
+    elif language == 'bg':
+        sid_bulgarian = SentimentIntensityAnalyzer()
+        sentiment_scores = sid_bulgarian.polarity_scores(text)
+
+    elif language == 'bn':
+        sid_Bengali = SentimentIntensityAnalyzer()
+        sentiment_scores = sid_Bengali.polarity_scores(text)
+
+    elif language == 'ca':
+        sid_Catalan = SentimentIntensityAnalyzer()
+        sentiment_scores = sid_Catalan.polarity_scores(text)
+
+    elif language == 'cs':
+        sid_Czech = SentimentIntensityAnalyzer()
+        sentiment_scores = sid_Czech.polarity_scores(text)
+
+    elif language == 'cy':
+        sid_Welsh = SentimentIntensityAnalyzer()
+        sentiment_scores = sid_Welsh.polarity_scores(text)
+
+    elif language == 'da':
+        sid_Danish = SentimentIntensityAnalyzer()
+        sentiment_scores = sid_Danish.polarity_scores(text)
+
+    elif language == 'de':
+        sid_German = SentimentIntensityAnalyzer()
+        sentiment_scores = sid_German.polarity_scores(text)
+
+    elif language == 'el':
+        sid_Czech = SentimentIntensityAnalyzer()
+        sentiment_scores = sid_Czech.polarity_scores(text)
+
+    elif language == 'et':
+        sid_Estonian = SentimentIntensityAnalyzer()
+        sentiment_scores = sid_Estonian.polarity_scores(text)
+
+    elif language == 'fa':
+        sid_Persian = SentimentIntensityAnalyzer()
+        sentiment_scores = sid_Persian.polarity_scores(text)
+
+    elif language == 'fi':
+        sid_Finnish = SentimentIntensityAnalyzer()
+        sentiment_scores = sid_Finnish.polarity_scores(text)
+
+    elif language == 'fr':
+        sid_French = SentimentIntensityAnalyzer()
+        sentiment_scores = sid_French.polarity_scores(text)
+
+    elif language == 'gu':
+        sid_Gujarati = SentimentIntensityAnalyzer()
+        sentiment_scores = sid_Gujarati.polarity_scores(text)
+
+    elif language == 'he':
+        sid_Hebrew = SentimentIntensityAnalyzer()
+        sentiment_scores = sid_Hebrew.polarity_scores(text)
+
+    elif language == 'hi':
+        sid_Hindi = SentimentIntensityAnalyzer()
+        sentiment_scores = sid_Hindi.polarity_scores(text)
+
+    elif language == 'hr':
+        sid_Croatian = SentimentIntensityAnalyzer()
+        sentiment_scores = sid_Croatian.polarity_scores(text)
+
+    elif language == 'hu':
+        sid_Hungarian = SentimentIntensityAnalyzer()
+        sentiment_scores = sid_Hungarian.polarity_scores(text)
+
+    elif language == 'id':
+        sid_Indonesian = SentimentIntensityAnalyzer()
+        sentiment_scores = sid_Indonesian.polarity_scores(text)
+
+    elif language == 'it':
+        sid_Italian = SentimentIntensityAnalyzer()
+        sentiment_scores = sid_Italian.polarity_scores(text)
+
+    elif language == 'ja':
+        sid_Japanese = SentimentIntensityAnalyzer()
+        sentiment_scores = sid_Japanese.polarity_scores(text)
+
+    elif language == 'kn':
+        sid_Kannada = SentimentIntensityAnalyzer()
+        sentiment_scores = sid_Kannada.polarity_scores(text)
+
+    elif language == 'ko':
+        sid_Korean = SentimentIntensityAnalyzer()
+        sentiment_scores = sid_Korean.polarity_scores(text)
+
+    elif language == 'lt':
+        sid_Lithuanian = SentimentIntensityAnalyzer()
+        sentiment_scores = sid_Lithuanian.polarity_scores(text)
+
+    elif language == 'lv':
+        sid_Latvian = SentimentIntensityAnalyzer()
+        sentiment_scores = sid_Latvian.polarity_scores(text)
+
+    elif language == 'mk':
+        sid_Macedonian = SentimentIntensityAnalyzer()
+        sentiment_scores = sid_Macedonian.polarity_scores(text)
+
+    elif language == 'ml':
+        sid_Malayalam = SentimentIntensityAnalyzer()
+        sentiment_scores = sid_Malayalam.polarity_scores(text)
+
+    elif language == 'mr':
+        sid_Marathi = SentimentIntensityAnalyzer()
+        sentiment_scores = sid_Marathi.polarity_scores(text)
+
+    elif language == 'ne':
+        sid_Nepali = SentimentIntensityAnalyzer()
+        sentiment_scores = sid_Nepali.polarity_scores(text)
+
+    elif language == 'nl':
+        sid_Dutch = SentimentIntensityAnalyzer()
+        sentiment_scores = sid_Dutch.polarity_scores(text)
+
+    elif language == 'no':
+        sid_Norwegian = SentimentIntensityAnalyzer()
+        sentiment_scores = sid_Norwegian.polarity_scores(text)
+
+    elif language == 'pa':
+        sid_Punjabi = SentimentIntensityAnalyzer()
+        sentiment_scores = sid_Punjabi.polarity_scores(text)
+
+    elif language == 'pl':
+        sid_Polish = SentimentIntensityAnalyzer()
+        sentiment_scores = sid_Polish.polarity_scores(text)
+
+    elif language == 'pt':
+        sid_Portuguese = SentimentIntensityAnalyzer()
+        sentiment_scores = sid_Portuguese.polarity_scores(text)
+
+    elif language == 'ro':
+        sid_Romanian = SentimentIntensityAnalyzer()
+        sentiment_scores = sid_Romanian.polarity_scores(text)
+
+    elif language == 'ru':
+        sid_Russian = SentimentIntensityAnalyzer()
+        sentiment_scores = sid_Russian.polarity_scores(text)
+
+    elif language == 'sk':
+        sid_Slovak = SentimentIntensityAnalyzer()
+        sentiment_scores = sid_Slovak.polarity_scores(text)
+
+    elif language == 'sl':
+        sid_Slovenian = SentimentIntensityAnalyzer()
+        sentiment_scores = sid_Slovenian.polarity_scores(text)
+
+    elif language == 'so':
+        sid_Somali = SentimentIntensityAnalyzer()
+        sentiment_scores = sid_Somali.polarity_scores(text)
+
+    elif language == 'sq':
+        sid_Albanian = SentimentIntensityAnalyzer()
+        sentiment_scores = sid_Albanian.polarity_scores(text)
+
+    elif language == 'sv':
+        sid_Swedish = SentimentIntensityAnalyzer()
+        sentiment_scores = sid_Swedish.polarity_scores(text)
+
+    elif language == 'sw':
+        sid_Swahili = SentimentIntensityAnalyzer()
+        sentiment_scores = sid_Swahili.polarity_scores(text)
+
+    elif language == 'ta':
+        sid_Tamil = SentimentIntensityAnalyzer()
+        sentiment_scores = sid_Tamil.polarity_scores(text)
+
+    elif language == 'te':
+        sid_Telugu = SentimentIntensityAnalyzer()
+        sentiment_scores = sid_Telugu.polarity_scores(text)
+
+    elif language == 'th':
+        sid_Thai = SentimentIntensityAnalyzer()
+        sentiment_scores = sid_Thai.polarity_scores(text)
+
+    elif language == 'tl':
+        sid_Tagalog = SentimentIntensityAnalyzer()
+        sentiment_scores = sid_Tagalog.polarity_scores(text)
+
+    elif language == 'tr':
+        sid_Turkish = SentimentIntensityAnalyzer()
+        sentiment_scores = sid_Turkish.polarity_scores(text)
+
+    elif language == 'uk':
+        sid_Ukrainian = SentimentIntensityAnalyzer()
+        sentiment_scores = sid_Ukrainian.polarity_scores(text)
+
+    elif language == 'ur':
+        sid_Urdu = SentimentIntensityAnalyzer()
+        sentiment_scores = sid_Urdu.polarity_scores(text)
+
+    elif language == 'vi':
+        sid_Vietnamese = SentimentIntensityAnalyzer()
+        sentiment_scores = sid_Vietnamese.polarity_scores(text)
+
+    elif language == 'zh-cn':
+        sid_ChineseSimplified = SentimentIntensityAnalyzer()
+        sentiment_scores = sid_ChineseSimplified.polarity_scores(text)
+
+    elif language == 'zh-tw':
+        sid_ChineseTraditional = SentimentIntensityAnalyzer()
+        sentiment_scores = sid_ChineseTraditional.polarity_scores(text)
+
+
+
 
     threshold_positive = float(threshold_positive)
     threshold_negative = float(threshold_negative)
